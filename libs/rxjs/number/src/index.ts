813db99193f6163f9cb312c7ff06b67b0394dc31
Tane Piper <me@tane.dev>
2020-04-22 22:58:54 +0200

    update exports

--- libs/rxjs/number/src/index.ts
+++ libs/rxjs/number/src/index.ts
@@ -1,2 +1,10 @@
+export { isFinite } from './lib/is-finite';
+export { isInteger } from './lib/is-integer';
+export { isNaN } from './lib/is-nan';
+export { isSafeInteger } from './lib/is-safe-integer';
 export { parseFloat } from './lib/parse-float';
 export { parseInt } from './lib/parse-int';
+export { toExponential } from './lib/to-exponential';
+export { toLocaleString } from './lib/to-locale-string';
+export { toPrecision } from './lib/to-precision';
+export { toString } from './lib/to-string';

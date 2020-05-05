850c8f7dcf66eb92b043e0afd16526d31965d8d6
Tane Piper <piper.tane@gmail.com>
2020-05-05 22:41:30 +0200

    Add test for debouce with query

--- libs/rxjs/utility/src/lib/debounce-with-query.spec.ts
+++ libs/rxjs/utility/src/lib/debounce-with-query.spec.ts
@@ -1,8 +1,15 @@
 import { debounceWithQuery } from './debounce-with-query';
+import { from, of } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
 
 // TODO: Write test
 describe('debounceWithQuery', () => {
-  it('should debounce a query for 1 second', done => {
-    done();
+  it('should debounce a query for 1 second', (done) => {
+    from(['t', 'te', 'tes', 'test'])
+      .pipe(debounceWithQuery(500, (query) => of(query)))
+      .subscribe({
+        next: (v) => expect(v).toBe('test'),
+        complete: () => done(),
+      });
   });
 });

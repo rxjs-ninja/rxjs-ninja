e277d1ec5a67c667041edf82353d1599c6e9f6ca
Tane Piper <tane-piper@idexx.com>
2020-05-09 22:55:00 +0200

    Don't generate converage for now

--- .github/scripts/run-testing.sh
+++ .github/scripts/run-testing.sh
@@ -2,12 +2,15 @@
 set -o errexit -o noclobber -o nounset -o pipefail
 
 RUN_ALL=${1:-"False"}
+WITH_COVERAGE=${2:-"True"}
+
+COVERAGE_RULE=$([ "$WITH_COVERAGE" == 'True' ] && echo "--codeCoverage" || echo "")
 
 echo "Running Unit Testing"
 if [ "$RUN_ALL" == "True" ]; then
-  npm run affected:test -- --codeCoverage --all
+  npm run affected:test -- "$COVERAGE_RULE" --all
 else
-  npm run affected:test -- --base="origin/master" --codeCoverage
+  npm run affected:test -- --base="origin/master" "$COVERAGE_RULE"
 fi
 echo "Unit Testing Complete"
 wait

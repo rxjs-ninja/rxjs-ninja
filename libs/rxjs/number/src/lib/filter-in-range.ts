f9923e79e0783323ca59105fd48ecac1ce6b9c9a
Tane Piper <tane-piper@idexx.com>
2020-05-12 01:44:00 +0200

    Provide overloaded inRange method

---
+++ libs/rxjs/number/src/lib/filter-in-range.ts
@@ -0,0 +1,35 @@
+/**
+ * @packageDocumentation
+ * @module number
+ */
+import { MonoTypeOperatorFunction, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
+
+/**
+ * The `filterIsFinite` operator can be used with an RxJS `pipe` where the source value
+ * is an [Observable](https://rxjs-dev.firebaseapp.com/guide/observable) number.
+ *
+ * The operator will return the number value based on it passing
+ * [Number.isFinite](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isFinite)
+ *
+ * @remarks
+ * If you want the boolean value instead of the number value use the [[isFinite]] operator instead
+ *
+ * @example
+ * ```ts
+ * from([1, 2, Infinity])
+ *  .pipe(
+ *    filterIsFinite(),
+ *    reduce((acc, val) => {
+ *      acc.push(val);
+ *      return acc;
+ *    }, [])
+ * ).subscribe(console.log) // [1, 2]
+ * ```
+ *
+ * @returns A number value that passes the `Number.isFinite` equality check
+ * @category RxJS Number Filter
+ */
+export function filterIsFinite(): MonoTypeOperatorFunction<number> {
+  return (source: Observable<number>) => source.pipe(filter((value) => Number.isFinite(value)));
+}
--- libs/rxjs/number/src/lib/in-range.ts
+++ libs/rxjs/number/src/lib/in-range.ts
@@ -1,7 +1,53 @@
-import { Observable } from 'rxjs';
+/**
+ * @packageDocumentation
+ * @module number
+ */
+import { MonoTypeOperatorFunction, Observable, OperatorFunction } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export function inRange(min: number, max: number, noBoundsMatch = false) {
+/**
+ * The `inRange` operator can be used with an RxJS `pipe` where the source value
+ * is an [Observable](https://rxjs-dev.firebaseapp.com/guide/observable) number.
+ *
+ * The operator will return the boolean if the source value is within a passed min and max range
+ *
+ * @param min The minimum number for the range
+ * @param max The maximum number for the range
+ *
+ * @remarks
+ * With the `noBoundsMatch` set to true the check will be `val > min && val < min`. The default is `val >= min && val <= max`
+ *
+ * @remarks
+ * If you want the number value instead of the boolean value use the [[filterIsFinite]] operator instead
+ *
+ * @example
+ * ```ts
+ * from([1, 20, 5, 10])
+ *  .pipe(inRange(0, 10))
+ *  .subscribe(console.log) // [true, false, true, true]
+ * ```
+ *
+ * @returns Boolean value if the number falls in the `min/max` range
+ */
+function inRange(min: number, max: number): OperatorFunction<number, boolean>;
+/**
+ *
+ * @param min The minimum number for the range
+ * @param max The maximum number for the range
+ * @param noBoundsMatch If set to true the min and max values are not included in the evaluation
+ * @example
+ * ```ts
+ * from([1, 20, 5, 10])
+ *  .pipe(inRange(0, 10, true))
+ *  .subscribe(console.log) // [true, false, true, false]
+ * ```
+ * @category RxJS Number Query
+ */
+function inRange(min: number, max: number, noBoundsMatch?: boolean): OperatorFunction<number, boolean>;
+function inRange(...args: any): OperatorFunction<number, boolean> {
+  const inArgs: any[] = [...args];
   return (source: Observable<number>) =>
-    source.pipe(map((value) => (noBoundsMatch ? value > min && value < max : value >= min && value <= max)));
+    source.pipe(map((value) => (inArgs[2] ? value > inArgs[0] && value < inArgs[1] : value >= inArgs[0] && value <= inArgs[1])));
 }
+
+export { inRange };

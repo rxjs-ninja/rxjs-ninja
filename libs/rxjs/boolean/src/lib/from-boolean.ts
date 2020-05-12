b956b37bb6545a11f2a3ddef1c35a2561315afa5
Tane Piper <me@tane.dev>
2020-05-12 16:54:30 +0200

    (fix): lastTruthy operator added, predicate support and improved documentation (#29)
    Fixing because of failed Github Action

--- libs/rxjs/boolean/src/lib/from-boolean.ts
+++ libs/rxjs/boolean/src/lib/from-boolean.ts
@@ -14,28 +14,54 @@ import { scheduleSingleOrArrayBoolean, subscribeToSingleOrArrayBoolean } from '.
  *
  * @param input Boolean input to create an Observable<boolean> from
  * @param scheduler The [SchedulerLike](https://rxjs-dev.firebaseapp.com/api/index/interface/SchedulerLike) to use for scheduling the emission of values, and providing a notion of "time"
-
+ *
  * @example
  * ```ts
- * fromBoolean(true).pipe(map(val => !val)).subscribe(console.log) // false
+ * fromBoolean(true)
+ *  .pipe(map(val => !val))
+ *  .subscribe(console.log) // false
  * ```
+ *
+ * @example
+ * ```ts
+ * fromBoolean<string>('')
+ *  .subscribe(console.log) // false
+ * ```
+ *
+ * @returns Boolean value from the initial parameters
+ * @category RxJS Boolean Observables
+ */
+function fromBoolean<T = boolean>(input: T, scheduler?: SchedulerLike): Observable<boolean>;
+/**
+ * Using `fromBoolean` with an array of booleans is the same as using the [from](https://rxjs-dev.firebaseapp.com/api/index/function/from) operator from RxJS
+ *
+ * @typeParam T The type or types to be used to create boolean values from
+ * @default boolean
+ *
+ * @param input Boolean Array input to create an Observable<boolean> from
+ * @param scheduler The [SchedulerLike](https://rxjs-dev.firebaseapp.com/api/index/interface/SchedulerLike) to use for scheduling the emission of values, and providing a notion of "time"
  *
  * @example
  * ```ts
  * fromBoolean<string>(['', 'foo', '', 'bar'])
- *  .pipe(reduce((acc, val) => acc + (val ? 1 : 0)))
- *  .subscribe(console.log) // 2
+ *  .subscribe(console.log) // [false, true, false, true]
  * ```
  *
- * @remarks
- * Using `fromBoolean` with an array of booleans is the same as using the [from](https://rxjs-dev.firebaseapp.com/api/index/function/from) operator from RxJS
+ * @example
+ * ```ts
+ * fromBoolean<string | number>([0, 1, '2.5'])
+ *  .subscribe(console.log) // [false, true, true]
+ * ```
  *
  * @returns Boolean value from the initial parameters
- * @category RxJS Boolean Creation
+ * @category RxJS Boolean Observables
  */
-export function fromBoolean<T = boolean>(input: T | T[], scheduler?: SchedulerLike): Observable<boolean> {
+function fromBoolean<T = boolean>(input: T[], scheduler?: SchedulerLike): Observable<boolean>;
+function fromBoolean<T = boolean>(input: T | T[], scheduler?: SchedulerLike): Observable<boolean> {
   if (scheduler) {
     return scheduleSingleOrArrayBoolean<T>(input, scheduler);
   }
   return new Observable<boolean>(subscribeToSingleOrArrayBoolean<T>(input));
 }
+
+export { fromBoolean };

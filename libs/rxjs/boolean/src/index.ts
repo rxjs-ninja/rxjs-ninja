1bd14f92fe2e5d665c3ea764334e36a35a00cd05
Tane Piper <tane-piper@idexx.com>
2020-05-12 16:30:10 +0200

    More boolean improvements

--- libs/rxjs/boolean/src/index.ts
+++ libs/rxjs/boolean/src/index.ts
@@ -6,3 +6,4 @@
 export { firstTruthy } from './lib/first-truthy';
 export { filterTruthy } from './lib/filter-truthy';
 export { fromBoolean } from './lib/from-boolean';
+export { lastTruthy } from './lib/last-truthy';

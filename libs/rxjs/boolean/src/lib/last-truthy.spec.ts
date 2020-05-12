0c83dfdb7f7cf158dfebedfb87a61a0502a2f905
Tane Piper <me@tane.dev>
2020-05-12 16:54:30 +0200

    (fix): lastTruthy operator added, predicate support and improved documentation (#29)
    Fixing because of failed Github Action

--- libs/rxjs/boolean/src/lib/last-truthy.spec.ts
+++ libs/rxjs/boolean/src/lib/last-truthy.spec.ts
@@ -1,13 +1,14 @@
 import { from } from 'rxjs';
 import { firstTruthy } from '@tinynodes/rxjs-boolean';
 import { fromBoolean } from './from-boolean';
+import { lastTruthy } from './last-truthy';
 
 describe('firstTruthy', () => {
   it('should return the first true value from a source', (done) => {
-    from([null, false, '', 'test'])
-      .pipe(firstTruthy())
+    from(['a', 'b', 'c'])
+      .pipe(lastTruthy())
       .subscribe({
-        next: (value) => expect(value).toBe('test'),
+        next: (value) => expect(value).toBe('c'),
         complete: () => done(),
       });
   });
@@ -15,8 +16,8 @@ describe('firstTruthy', () => {
   it('should return no value if no truthy value', (done) => {
     let called = 0;
 
-    from([null, false, ''])
-      .pipe(firstTruthy())
+    from(['', '', ''])
+      .pipe(lastTruthy())
       .subscribe({
         next: () => called++,
         complete: () => {
@@ -28,9 +29,9 @@ describe('firstTruthy', () => {
 
   it('should support return value using a predicate method', (done) => {
     from([1, 2, 3, 4])
-      .pipe(firstTruthy((val) => val % 2 === 0))
+      .pipe(lastTruthy((val) => val % 2 === 0))
       .subscribe({
-        next: (value) => expect(value).toBe(2),
+        next: (value) => expect(value).toBe(4),
         complete: () => done(),
       });
   });
---
+++ libs/rxjs/boolean/src/types/boolean.ts
@@ -0,0 +1,19 @@
+/**
+ * @packageDocumentation
+ * @module boolean
+ */
+
+/**
+ * A predicate function that take a value from `filter` and returns a boolean
+ * based on an equality check
+ *
+ * @typeParam T The type of the value to do an equality check with
+ *
+ * @example
+ * ```
+ * const isEvenNumber: PredicateFn<number> = (num: number) => num % 2 === 0
+ * ```
+ *
+ * @returns Boolean value
+ */
+export type PredicateFn<T> = (value: T) => boolean;

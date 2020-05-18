5774115ebaee7ff617b080c0b7f3dafca970fdb9
Tane Piper <tane-piper@idexx.com>
2020-05-18 16:53:21 +0200

    Added the following operators:
    * matchAll
    * normalize
    * repeat
    * fromUnicode

---
+++ libs/rxjs/string/src/lib/from-unicode.ts
@@ -0,0 +1,49 @@
+/**
+ * @packageDocumentation
+ * @module string
+ */
+import { Observable, of, SchedulerLike } from 'rxjs';
+import { FormType } from '../types/normalize';
+import { scheduleSingleOrArrayUnicode, subscribeToSingleOrArrayUnicode } from '../utils/from-unicode.utils';
+
+/**
+ * Takes a string containing the Unicode Normalization Form and uses [String.prototype.normalize](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize)
+ * to convert it to a string with unicode characters
+ *
+ * @param input A string of unicode characters
+ * @param form The Unicode Normalization Form to decode the string with
+ * @param scheduler Optional [SchedulerLike](https://rxjs-dev.firebaseapp.com/api/index/interface/SchedulerLike)
+ *
+ * @example
+ * ```ts
+ * fromUnicode('\u0041\u006d\u00e9\u006c\u0069\u0065')
+ *  .subscribe(console.log) // Amélie
+ * ```
+ *
+ * @returns String from the decoded unicode string
+ * @category RxJS String Creation
+ */
+function fromUnicode(input: string, form?: FormType, scheduler?: SchedulerLike): Observable<string>;
+/**
+ * @param input An array of strings of unicode characters
+ * @param form The Unicode Normalization Form to decode the string with
+ * @param scheduler Optional [SchedulerLike](https://rxjs-dev.firebaseapp.com/api/index/interface/SchedulerLike)
+ *
+ * @example
+ * ```ts
+ * fromUnicode(['\u0041\u006d\u00e9\u006c\u0069\u0065', '\u0041\u006d\u0065\u0301\u006c\u0069\u0065'])
+ *  .subscribe(console.log) // ['Amélie', 'Amélie']
+ * ```
+ *
+ * @returns String from the decoded unicode string
+ * @category RxJS String Creation
+ */
+function fromUnicode(input: string[], form?: FormType, scheduler?: SchedulerLike): Observable<string>;
+function fromUnicode(input: string | string[], form?: FormType, scheduler?: SchedulerLike): Observable<string> {
+  if (scheduler) {
+    return scheduleSingleOrArrayUnicode(input, form, scheduler);
+  }
+  return new Observable<string>(subscribeToSingleOrArrayUnicode(input, form));
+}
+
+export { fromUnicode };
---
+++ libs/rxjs/string/src/lib/repeat.spec.ts
@@ -0,0 +1,23 @@
+import { take } from 'rxjs/operators';
+import { fromString, replace } from '@tinynodes/rxjs-string';
+import { repeat } from './repeat';
+
+describe('replace', () => {
+  it('should take a string and replace a string pattern', (done) => {
+    fromString('foobar')
+      .pipe(repeat(5), take(1))
+      .subscribe({
+        next: (value) => expect(value).toBe('foobarfoobarfoobarfoobarfoobar'),
+        complete: () => done(),
+      });
+  });
+
+  it('should take a string and replace a regex pattern', (done) => {
+    fromString('foobar')
+      .pipe(repeat(5, ','), take(1))
+      .subscribe({
+        next: (value) => expect(value).toBe('foobar,foobar,foobar,foobar,foobar'),
+        complete: () => done(),
+      });
+  });
+});

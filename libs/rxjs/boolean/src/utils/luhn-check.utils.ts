eb73689f22b2c35e5099bdc605605238354e6f3d
Tane Piper <tane-piper@idexx.com>
2020-05-24 16:04:30 +0200

    Doc fix

--- libs/rxjs/boolean/src/utils/luhn-check.utils.ts
+++ libs/rxjs/boolean/src/utils/luhn-check.utils.ts
@@ -1,3 +1,14 @@
+/**
+ * @packageDocumentation
+ * @module boolean
+ */
+
+/**
+ * Takes a string or number and converts to an array of numbers in reverse order
+ * @param value
+ * @private
+ * @internal
+ */
 export function reverseLuhnNumbers(value: number | string): number[] {
   return (typeof value === 'number' ? value.toString(10) : value)
     .split('')
@@ -8,9 +19,12 @@ export function reverseLuhnNumbers(value: number | string): number[] {
 /**
  * Generate a moulus check number from input
  * @param valueArray
+ * @private
+ * @internal
  */
 export function createLuhnModulus(valueArray: number[]): number {
   const lastDigit = valueArray.splice(0, 1)[0];
+  /* istanbul ignore next */
   const sum = valueArray.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
   return sum + lastDigit;
 }

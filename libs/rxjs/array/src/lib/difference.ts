f1111f9a1ff4f00ceaf7bcc6f910b4a0f53b050a
Tane Piper <tane-piper@idexx.com>
2020-11-16 13:40:35 +0100

    Update doc path

--- libs/rxjs/array/src/lib/difference.ts
+++ libs/rxjs/array/src/lib/difference.ts
@@ -17,7 +17,8 @@ import { mapDifference } from '../utils/difference';
  * @typeParam T The type of data in the input array
  * @typeParam K The type of data if [[InputModifierFn]] converts the type
  *
- * @param input An Array of items use to get the difference between two arrays
+ * @param input An Observable Array of items use to get the difference between two arrays
+ * @param inputModifier A function that provide modification of the values to do the comparision with
  *
  * @example
  * ```ts
@@ -26,13 +27,6 @@ import { mapDifference } from '../utils/difference';
  *  .subscribe(console.log) // ['b', 'd']
  * ```
  *
- * @returns Array of values of difference between the source and input array
- * @category RxJS Array Difference
- */
-function difference<T>(input: T[]): MonoTypeOperatorFunction<T[]>;
-/**
- * @param input An Observable Array of items use to get the difference between two arrays
- *
  * @example
  * ```ts
  * of(['a', 'b', 'd'])
@@ -40,28 +34,12 @@ function difference<T>(input: T[]): MonoTypeOperatorFunction<T[]>;
  *  .subscribe(console.log) // ['b', 'd']
  * ```
  *
- * @returns Array of values of difference between the source and input array
- * @category RxJS Array Difference
- */
-function difference<T>(input: ObservableInput<T[]>): MonoTypeOperatorFunction<T[]>;
-/**
- * @param input An Observable Array of items use to get the difference between two arrays
- * @param inputModifier A function that provide modification of the values to do the comparision with
- *
  * @example
  * ```ts
  * of(['a', 'b', 'd'])
  *  .pipe(difference(['A', 'C'], (value) => value.toUpperCase()))
  *  .subscribe(console.log) // ['b', 'd']
  * ```
- *
- * @returns Array of values of difference between the source and input array
- * @category RxJS Array Difference
- */
-function difference<T, K>(input: T[], inputModifier: InputModifierFn<T, T | K>): MonoTypeOperatorFunction<T[]>;
-/**
- * @param input An Observable Array of items use to get the difference between two arrays
- * @param inputModifier A function that provide modification of the values to do the comparision with
  *
  * @example
  * ```ts
@@ -73,10 +51,6 @@ function difference<T, K>(input: T[], inputModifier: InputModifierFn<T, T | K>):
  * @returns Array of values of difference between the source and input array
  * @category RxJS Array Difference
  */
-function difference<T, K>(
-  input: ObservableInput<T[]>,
-  inputModifier: InputModifierFn<T, T | K>,
-): MonoTypeOperatorFunction<T[]>;
 function difference<T, K>(
   input: T[] | ObservableInput<T[]>,
   inputModifier?: InputModifierFn<T, T | K>,

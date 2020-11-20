1619ef25da593475cc4d3bc8110af52712b0fa84
Tane Piper <me@tane.dev>
2020-11-20 16:21:01 +0100

    Add support to fromRandomCrypto for other TypedArray types

--- libs/rxjs/random/src/lib/from-random-crypto.spec.ts
+++ libs/rxjs/random/src/lib/from-random-crypto.spec.ts
@@ -1,29 +1,105 @@
+import { getRandomValues } from '@trust/webcrypto';
 import { observe } from 'rxjs-marbles/jest';
 import { reduce, take, tap } from 'rxjs/operators';
 import { fromRandomCrypto } from '@tinynodes/rxjs-random';
-import TypedArray = NodeJS.TypedArray;
 
 describe('fromRandomCrypto', () => {
   beforeAll(() => {
-    (window as any)['crypto'] = window.crypto || {
-      getRandomValues: (arr: TypedArray) => {
-        const result = [];
-        for (let i = 0; i < arr.length; i++) {
-          result.push(Math.random());
-        }
-        arr.set(result);
-      },
-    };
+    (window as any)['crypto'] = window.crypto || { getRandomValues };
   });
 
   it(
-    'should create an array of random numbers',
-    observe(() =>
-      fromRandomCrypto().pipe(
+    'should create an array of random 4 bit numbers by default',
+    observe(() => {
+      return fromRandomCrypto().pipe(
         take(5),
         reduce<number, number[]>((acc, val) => [...acc, val], []),
         tap((value) => {
-          const check = value.filter((v) => typeof v === 'number');
+          const check = value.filter((v) => typeof v === 'number' && Number.isInteger(v));
+          expect(check.length).toBe(5);
+        }),
+      );
+    }),
+  );
+
+  it(
+    'should create an array of random 1 bit integer',
+    observe(() =>
+      fromRandomCrypto(0, { bytes: 1 }).pipe(
+        take(5),
+        reduce<number, number[]>((acc, val) => [...acc, val], []),
+        tap((value) => {
+          const check = value.filter((v) => typeof v === 'number' && Number.isInteger(v));
+          expect(check.length).toBe(5);
+        }),
+      ),
+    ),
+  );
+
+  it(
+    'should create an array of random 1 bit unsigned integer',
+    observe(() =>
+      fromRandomCrypto(0, { bytes: 1, unsigned: true }).pipe(
+        take(5),
+        reduce<number, number[]>((acc, val) => [...acc, val], []),
+        tap((value) => {
+          const check = value.filter((v) => typeof v === 'number' && Number.isInteger(v));
+          expect(check.length).toBe(5);
+        }),
+      ),
+    ),
+  );
+
+  it(
+    'should create an array of random 2 bit integer',
+    observe(() =>
+      fromRandomCrypto(0, { bytes: 2 }).pipe(
+        take(5),
+        reduce<number, number[]>((acc, val) => [...acc, val], []),
+        tap((value) => {
+          const check = value.filter((v) => typeof v === 'number' && Number.isInteger(v));
+          expect(check.length).toBe(5);
+        }),
+      ),
+    ),
+  );
+
+  it(
+    'should create an array of random 2 bit unsigned integer',
+    observe(() =>
+      fromRandomCrypto(0, { bytes: 2, unsigned: true }).pipe(
+        take(5),
+        reduce<number, number[]>((acc, val) => [...acc, val], []),
+        tap((value) => {
+          const check = value.filter((v) => typeof v === 'number' && Number.isInteger(v));
+          expect(check.length).toBe(5);
+        }),
+      ),
+    ),
+  );
+
+  it(
+    'should create an array of random 4 bit integer',
+    observe(() =>
+      fromRandomCrypto(0, { bytes: 4 }).pipe(
+        take(5),
+        reduce<number, number[]>((acc, val) => [...acc, val], []),
+        tap((value) => {
+          const check = value.filter((v) => typeof v === 'number' && Number.isInteger(v));
+          expect(check.length).toBe(5);
+        }),
+      ),
+    ),
+  );
+
+  it(
+    'should create an array of random 4 bit unsigned integer',
+    observe(() =>
+      fromRandomCrypto(0, { bytes: 4, unsigned: true }).pipe(
+        take(5),
+        reduce<number, number[]>((acc, val) => [...acc, val], []),
+        tap((value) => {
+          const check = value.filter((v) => typeof v === 'number' && Number.isInteger(v));
           expect(check.length).toBe(5);
         }),
       ),

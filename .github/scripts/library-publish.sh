c916230d97530f96ec7a3150260efa861d6ff5d0
Tane Piper <me@tane.dev>
2020-11-22 23:32:33 +0100

    (patch) Doc Updates for new repo and docs page (#43)
    * Updated Documentation for new repo and page urls
    * Updated Readme Files for NPM
    * Update main readme

--- .github/scripts/library-publish.sh
+++ .github/scripts/library-publish.sh
@@ -15,8 +15,7 @@ function getBuildType {
 
 PARENT_DIR="$PWD"
 ROOT_DIR="."
-echo "Removing Dist"
-rm -rf "${ROOT_DIR:?}/dist"
+
 COMMIT_MESSAGE="$(git log -1 --pretty=format:"%s")"
 RELEASE_TYPE=${1:-$(getBuildType "$COMMIT_MESSAGE")}
 DRY_RUN=${DRY_RUN:-"False"}
@@ -25,29 +24,6 @@ IGNORE=$(echo "$COMMIT_MESSAGE" | sed -nE "s/^.*\[ignore:(.+)\]$/\1/p")
 if [[ "$IGNORE" != "" ]]; then
   echo "Ignoring: $IGNORE"
 fi
-
-# Version the parent library
-# npm --no-git-tag-version version "$RELEASE_TYPE" -f -m "RxJS Ninja $RELEASE_TYPE"
-# Get the version to set on sub-libraries
-# VERSION="$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' "$ROOT_DIR/package.json")"
-# echo "RxJS Ninja $RELEASE_TYPE - $VERSION."
-
-function doBuilds {
-  while IFS= read -r -d $' ' lib; do
-    if [[ "$IGNORE" == *"$lib"* ]]; then
-      echo "Skipping $lib"
-    else
-      echo "Setting version for $lib"
-      cd "$PARENT_DIR"
-      cd "$ROOT_DIR/libs/${lib/-//}"
-      npm version "$RELEASE_TYPE" -f -m "RxJS Ninja $RELEASE_TYPE"
-      echo "Building $lib"
-      cd "$PARENT_DIR"
-      npm run build "$lib" -- --prod --with-deps
-      wait
-    fi
-  done <<<"$1 " # leave space on end to generate correct output
-}
 
 function doPublish {
   while IFS= read -r -d $' ' lib; do
@@ -61,18 +37,12 @@ function doPublish {
   done <<<"$1 " # leave space on end to generate correct output
 }
 
-AFFECTED=$(node node_modules/.bin/nx affected:libs --plain --base=origin/master~1)
+AFFECTED=$(node node_modules/.bin/nx affected:libs --plain --base=origin/main~1)
 
 if [[ "$AFFECTED" != "" ]]; then
   cd "$PARENT_DIR"
-  echo "Copy Environment Files"
-
-  doBuilds "$AFFECTED"
-  wait
-  cd "$PARENT_DIR"
   doPublish "$AFFECTED"
   wait
-
 else
   echo "No Libraries to publish"
 fi
--- libs/rxjs/string/README.md
+++ libs/rxjs/string/README.md
@@ -1,13 +1,25 @@
-# RxJS Ninja - String Operators
+# RxJS Ninja - Strings
 
-![The RXJS Ninja Logo](https://raw.githubusercontent.com/rxjs-ninja/rxjs-ninja/master/assets/logo.png)
+![The RXJS Ninja Logo](https://raw.githubusercontent.com/rxjs-ninja/rxjs-ninja/main/assets/logo.png)
 
-[RxJS Ninja](http://rxjs.ninja) is a set of utility operators and observables for use with the [RxJS](https://rxjs.dev) library.
+[![rxjs-string](https://img.shields.io/npm/v/@rxjs-ninja/rxjs-string?label=@rxjs-ninja/rxjs-string)](https://www.npmjs.com/package/@rxjs-ninja/rxjs-string)
 
-The String library is for use with generating and piping string values - more details are in the [Full API](https://rxjs.ninja/modules/string.html)
+[Website](https://rxjs.ninja)
+|
+[Full API](https://rxjs.ninja/modules/string.html)
+|
+[Changelog](https://github.com/rxjs-ninja/rxjs-ninja/blob/main/libs/rxjs/string/CHANGELOG.md)
 
-[![rxjs-string](https://img.shields.io/npm/v/@rxjs-ninja/rxjs-string?label=rxjs-string)](https://www.npmjs.com/package/@rxjs-ninja/rxjs-string)
+`@rxjs-ninja/rxjs-string` provides operators for querying, filtering and modifying string values, and Observable for generating string emitters.
 
-## How to install
+For example, you can use the `fromString` to generate a sequence of strings and check they include
+a string using `include`. We can also `titlize` strings.
 
-> `npm install @rxjs-ninja/rxjs-boolean`
+```ts
+import { fromString, includes, titleize } from '@rxjs-ninja/rxjs-string';
+
+const inputObs$ = fromNumber(['full power', 'half power', 'quarter power']);
+
+inputObs$.pipe(includes('half')).subscribe(); // false, true, false
+inputObs$.pipe(titleize()).subscribe(); // Full Power, Half Power, Quarter Power
+```

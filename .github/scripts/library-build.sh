59114393a5ec488bed9fc06f38a52f414327b814
Tane Piper <tane-piper@idexx.com>
2020-11-22 23:49:59 +0100

    Fix pipeline

--- .github/scripts/library-build.sh
+++ .github/scripts/library-build.sh
@@ -1,49 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit -o noclobber -o nounset -o pipefail
 
-# This script uses the parent version as the version to publish a library with
-
-function getBuildType {
-  local release_type="minor"
-  if [[ "$1" == *"(major)"* ]]; then
-    release_type="major"
-  elif [[ "$1" == *"(patch)"* ]]; then
-    release_type="patch"
-  fi
-  echo "$release_type"
-}
-
 PARENT_DIR="$PWD"
-ROOT_DIR="."
-COMMIT_MESSAGE="$(git log -1 --pretty=format:"%s")"
-RELEASE_TYPE=${1:-$(getBuildType "$COMMIT_MESSAGE")}
-
-DRY_RUN=${DRY_RUN:-"False"}
-
-IGNORE=$(echo "$COMMIT_MESSAGE" | sed -nE "s/^.*\[ignore:(.+)\]$/\1/p")
-
-if [[ "$IGNORE" != "" ]]; then
-  echo "Ignoring: $IGNORE"
-fi
-
-function doBuilds {
-  while IFS= read -r -d $' ' lib; do
-    if [[ "$IGNORE" == *"$lib"* ]]; then
-      echo "Skipping $lib"
-    else
-      echo "Building $lib"
-      cd "$PARENT_DIR"
-      npm run build "$lib"
-      wait
-    fi
-  done <<<"$1 " # leave space on end to generate correct output
-}
 
 AFFECTED=$(node node_modules/.bin/nx affected:libs --plain --base=origin/main~1)
+echo "Will Build: $AFFECTED"
 
 if [[ "$AFFECTED" != "" ]]; then
   cd "$PARENT_DIR"
-  doBuilds "$AFFECTED"
+  npm run affected:build -- --skip-nx-cache --base=origin/main~1
   wait
 else
   echo "No Libraries to build"

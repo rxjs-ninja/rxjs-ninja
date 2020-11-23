7b29eb6075806d83d024e6802f137854fff1c520
Tane Piper <tane-piper@idexx.com>
2020-11-23 16:16:22 +0100

    Changelog file

---
+++ .github/scripts/changelog-version.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -o errexit -o noclobber -o nounset -o pipefail
+
+# This script uses the parent version as the version to publish a library with
+
+function getBuildType {
+  local release_type="minor"
+  if [[ "$1" == *"(major)"* ]]; then
+    release_type="major"
+  elif [[ "$1" == *"(patch)"* ]]; then
+    release_type="patch"
+  fi
+  echo "$release_type"
+}
+
+PARENT_DIR="$PWD"
+ROOT_DIR="."
+BASE="origin/main~1"
+
+COMMIT_MESSAGE="$(git log -1 --pretty=format:"%s")"
+RELEASE_TYPE=${1:-$(getBuildType "$COMMIT_MESSAGE")}
+DRY_RUN=${DRY_RUN:-"False"}
+
+IGNORE=$(echo "$COMMIT_MESSAGE" | sed -nE "s/^.*\[ignore:(.+)\]$/\1/p")
+if [[ "$IGNORE" != "" ]]; then
+  echo "Ignoring: $IGNORE"
+fi
+
+function doChangelog {
+  while IFS= read -r -d $' ' lib; do
+    cd "$PARENT_DIR"
+    if [[ "$DRY_RUN" == "False" || "$IGNORE" != *"$lib"* ]]; then
+      echo "Updating Changelog: $lib"
+      cd "$ROOT_DIR/libs/${lib/-//}"
+      local CURRENT_VERSION=$(jq -r '.version' package.json)
+      echo "$CURRENT_VERSION"
+    else
+      echo "Dry Run, not publishing $lib"
+    fi
+    wait
+  done <<<"$1 " # leave space on end to generate correct output
+}
+
+AFFECTED=$(node node_modules/.bin/nx affected:libs --plain --base="$BASE")
+echo "Will Publish: $AFFECTED"
+
+if [[ "$AFFECTED" != "" ]]; then
+  cd "$PARENT_DIR"
+  doChangelog "$AFFECTED"
+  wait
+else
+  echo "No Libraries to publish"
+fi

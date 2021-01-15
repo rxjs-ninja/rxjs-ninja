71171077c5f076e9922e0a18b01ad0ba09970df7
Tane Piper <tane-piper@idexx.com>
2021-01-15 20:52:56 +0100

    Fix coverage locations

--- libs/rxjs/array/jest.config.js
+++ libs/rxjs/array/jest.config.js
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/rxjs-array',
+  coverageDirectory: '../../../coverage/libs/rxjs/array',
 };

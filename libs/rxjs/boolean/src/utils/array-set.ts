e3738966e689bfc857298e39983942ada75166f0
Tane Piper <me@tane.dev>
2021-01-17 13:41:07 +0100

    Fix import issue

--- libs/rxjs/boolean/src/utils/array-set.ts
+++ libs/rxjs/boolean/src/utils/array-set.ts
@@ -3,13 +3,15 @@
  * @module Array
  */
 
+import { ArrayOrSet } from '../types/array-set';
+
 /**
  * Returns if the input is an array or
  * @private
  * @internal
  * @param input
  */
-export function isArrayOrSet(input: unknown): input is ArrayLike<unknown> {
+export function isArrayOrSet(input: unknown): input is ArrayOrSet<unknown> {
   if (Array.isArray(input)) {
     return true;
   } else return input instanceof Set;

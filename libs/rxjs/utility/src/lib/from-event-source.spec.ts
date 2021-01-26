ac5d9b50f03dc8dc51653738546313d786b44e45
Tane Piper <tane-piper@idexx.com>
2021-01-26 12:33:30 +0100

    Docs for event source

--- libs/rxjs/utility/src/lib/from-event-source.spec.ts
+++ libs/rxjs/utility/src/lib/from-event-source.spec.ts
@@ -3,8 +3,8 @@
 import EventSource, { sources } from 'eventsourcemock';
 import { observe } from 'rxjs-marbles/jest';
 import { fromEventSource } from '@rxjs-ninja/rxjs-utility';
-import { catchError, take, tap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { catchError, take, tap, withLatestFrom } from 'rxjs/operators';
+import { BehaviorSubject, of } from 'rxjs';
 
 describe('fromEventSource', () => {
   let source: EventSource;
@@ -24,16 +24,16 @@ describe('fromEventSource', () => {
     'should subscribe to a default event source message',
     observe(() => {
       const event = new MessageEvent('message', {
-        data: 'This is a test',
+        data: '{"value": "This is a test"}',
       });
 
       setTimeout(() => {
         sources['test.js'].emit(event.type, event);
       }, 1000);
 
-      return fromEventSource<string>(source).pipe(
+      return fromEventSource<Record<string, string>>(source).pipe(
         take(1),
-        tap((value) => expect(value.data).toBe('This is a test')),
+        tap((result) => expect(result).toBe('{"value": "This is a test"}')),
       );
     }),
   );
@@ -42,11 +42,11 @@ describe('fromEventSource', () => {
     'should subscribe to a custom event source message',
     observe(() => {
       const event1 = new MessageEvent('foobar', {
-        data: 'Foobar event',
+        data: '{"value": "This is a foobar test"}',
       });
 
       const event2 = new MessageEvent('custom', {
-        data: 'This is a test',
+        data: '{"value": "This is a custom test"}',
       });
 
       setTimeout(() => {
@@ -54,25 +54,46 @@ describe('fromEventSource', () => {
         sources['test.js'].emit(event2.type, event2);
       }, 1000);
 
-      return fromEventSource<string>(source, 'custom').pipe(
+      return fromEventSource<Record<string, string>>(source, 'custom').pipe(
         take(1),
-        tap((value) => expect(value.data).toBe('This is a test')),
+        tap((result) => expect(result).toBe('{"value": "This is a custom test"}')),
       );
     }),
   );
 
-  it(
-    'should emit an error if thrown from the emitter',
+  xit(
+    'should signal when the event emitter has been opened',
     observe(() => {
-      const event = new MessageEvent('error', {
-        data: 'This is an error'
+      const event = new MessageEvent('message', {
+        data: '{"value": "This is a test"}',
       });
 
       setTimeout(() => {
+        sources['test.js'].emitOpen();
         sources['test.js'].emit(event.type, event);
       }, 1000);
 
-      return fromEventSource<string>(source).pipe(
+      const opened$ = new BehaviorSubject<any>(undefined);
+
+      return fromEventSource<Record<string, string>>(source, 'message', opened$).pipe(
+        take(1),
+        withLatestFrom(opened$.asObservable()),
+        tap(([message, opened]) => {
+          console.log('opened', message, opened);
+          expect(opened).toBeInstanceOf(Event);
+        }),
+      );
+
+      //return opened$.asObservable().pipe(tap((value) => );
+    }),
+  );
+
+  xit(
+    'should emit an error if thrown from the emitter',
+    observe(() => {
+      source = new EventSource('test');
+
+      return fromEventSource<Record<string, string>>(source).pipe(
         catchError((err) => {
           expect(err).toBe('This is a error');
           return of(true);
--- libs/rxjs/utility/src/lib/from-event-source.ts
+++ libs/rxjs/utility/src/lib/from-event-source.ts
@@ -2,40 +2,94 @@
  * @packageDocumentation
  * @module Utility
  */
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 /**
- * Returns an Observable that emits values from an EventSource of the passed `eventName`
+ * Returns an Observable that emits values from an EventSource subscribing to the the passed
+ * `eventName` stream.  Takes an optional Observer (e.g. Subject) to emit when the stream opens
  *
  * @category Streams
  *
- * @param source The URL of the EventStream to connect to
+ * @typeParam T The type of the value in the message `data` property
+ *
+ * @param source The event source to subscribe to
  * @param eventName The name of the event to listen to, by default this is `message`
- * @param signal Optional signal to
+ * @param openObserver Optional observer that is emitted when the event source is opened
+ * @param signal Optional signal to end the event source
+ *
+ * @example
+ * Subscribe to an EventSource, listen for it opening and provide a stop signal
+ * ```ts
+ * // The event source emits a time every 1 minute
+ * const eventSource = new EventSource('/event-stream');
+ * const stopSource = new AbortController();
+ * const isOpen$ = new Subject<Event>();
+ *
+ * function endSource() {
+ *   stopSource.abort();
+ * }
+ *
+ * fromEventSource<string>(eventSource, 'message', isOpen$, stopSource.signal).pipe(
+ *  tap(value => {
+ *    const parsed = JSON.parse(value);
+ *    outputSpan.innerHTML = `The time is ${parsed.message}`
+ *  }),
+ *  finalize(() => {
+ *    outputSpan.innerHTML = `EventSource closed`
+ *  })
+ * ).subscribe();
+ * ```
+ * Output: `'The time is 12:01', 'The time is 12:02', ....`
+ *
+ * @returns Observable that emits the `data` value from an EventSource message
  */
 export function fromEventSource<T extends unknown>(
   source: EventSource,
   eventName = 'message',
+  openObserver?: Observer<Event>,
   signal?: AbortSignal,
-): Observable<MessageEvent<T>> {
-  return new Observable<MessageEvent<T>>((subscriber) => {
+): Observable<T> {
+  return new Observable<T>((subscriber) => {
     if (signal) {
       signal.onabort = () => {
         source.close();
-        subscriber.complete();
+        !subscriber.closed && subscriber.complete();
       };
     }
 
+    /**
+     * @private
+     * @internal
+     * @param event
+     */
     function handleMessage(event: Event) {
-      if (!eventName) {
-        subscriber.next(event as MessageEvent<T>);
-      } else if (eventName && eventName === event.type) {
-        subscriber.next(event as MessageEvent<T>);
+      if (!eventName || (eventName && eventName === event.type)) {
+        subscriber.next((event as MessageEvent).data);
       }
     }
 
+    /**
+     * @private
+     * @internal
+     * @param event
+     */
     function handleError(event: Event) {
-      subscriber.error(event as MessageEvent<T>);
+      subscriber.error((event as ErrorEvent).message);
+    }
+
+    /**
+     * @private
+     * @internal
+     * @param event
+     */
+    function handleOpen(event: Event) {
+      openObserver?.next(event);
+      openObserver?.complete();
+      source.removeEventListener('open', handleOpen);
+    }
+
+    if (openObserver) {
+      source.addEventListener('open', handleOpen);
     }
 
     source.addEventListener(eventName, handleMessage);
@@ -44,6 +98,8 @@ export function fromEventSource<T extends unknown>(
     return () => {
       source.removeEventListener(eventName, handleMessage);
       source.removeEventListener('error', handleError);
+      source.removeEventListener('open', handleOpen);
+
       source.close();
       !subscriber.closed && subscriber.complete();
     };

d05f17686ffed6f7f7c3bec98e0850ea705b7fe5
Tane Piper <tane-piper@idexx.com>
2021-01-31 11:56:40 +0100

    Changelog, docs and some cleanup

--- libs/rxjs/utility/src/lib/from-web-serial.ts
+++ libs/rxjs/utility/src/lib/from-web-serial.ts
@@ -12,6 +12,15 @@ import { toWritableStream } from './to-writable-stream';
  * {@link https://reillyeon.github.io/serial|Web Serial API}. The function can also accept an Observable
  * that emits values to write to the serial device, allowing two-way communication.
  *
+ * Both the input and output values must be
+ * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array|Uint8Array}, you
+ * can use {@link https://developer.mozilla.org/en-us/docs/Web/API/TextEncoder|TextEncoder} and
+ * {@link https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder|TextDecoder} to convert between strings, which
+ * can  be seen in the demo
+ *
+ * The function will also handle opening and closing of the port from the serial device when using an `AbortSignal` or
+ * ending the RxJS subscription.
+ *
  * @category Streams
  *
  * @remarks Web Serial is available in Chrome or Edge 89 or later, in earlier versions it can be enabled using the
@@ -24,7 +33,7 @@ import { toWritableStream } from './to-writable-stream';
  *
  * @param port The SerialPort object to connect to
  * @param writerSource Optional Observable source to emit values to the serial connection writer
- * @param options Options for the connection - default is `baudRate` of `9600`
+ * @param options Options for the connection - if none passed a default `baudRate` of `9600` is set
  * @param signal Optional signal to end the source
  *
  * @returns Observable that emits the output from a serial source
@@ -32,34 +41,48 @@ import { toWritableStream } from './to-writable-stream';
 export function fromWebSerial(
   port: SerialPort,
   writerSource?: Observable<Uint8Array>,
-  options: SerialOptions = { baudRate: 9600 },
+  options?: SerialOptions,
   signal?: AbortSignal,
 ): Observable<Uint8Array> {
   return new Observable<Uint8Array>((subscriber) => {
-    console.log('new subscription');
     const closeStreams$ = new Subject<void>();
+    let reader: ReadableStreamDefaultReader<Uint8Array>;
+    let writer: WritableStreamDefaultWriter<Uint8Array>;
 
-    from(port.open(options))
+    // Allow for undefined options
+    from(port.open(options || { baudRate: 9600 }))
       .pipe(
         tap(() => {
-          let reader: ReadableStreamDefaultReader<Uint8Array>;
-          let writer: WritableStreamDefaultWriter<Uint8Array>;
-
+          /**
+           * The next block deals specifically with handling the closing of all internal and external streams
+           */
           closeStreams$
             .asObservable()
             .pipe(
               tap(async () => {
-                console.log('here');
                 await writer.close();
-                //writer.releaseLock();
                 await reader.cancel();
-                //reader.releaseLock();
                 await port.close();
                 !subscriber.closed && subscriber.complete();
               }),
             )
             .subscribe();
 
+          if (signal) {
+            signal.onabort = () => {
+              closeStreams$.next();
+              closeStreams$.complete();
+            };
+          }
+
+          port.ondisconnect = () => {
+            closeStreams$.next();
+            closeStreams$.complete();
+          };
+
+          /**
+           * Set up the writer to the serial device
+           */
           if (writerSource && port.writable) {
             writer = port.writable.getWriter();
 
@@ -75,18 +98,12 @@ export function fromWebSerial(
               .subscribe();
           }
 
-          if (signal) {
-            signal.onabort = () => {
-              closeStreams$.next();
-              closeStreams$.complete();
-            };
-          }
-
-          port.ondisconnect = () => {
-            closeStreams$.next();
-            closeStreams$.complete();
-          };
-
+          /**
+           * Loop over the promise response from the reader until it's done or the port is no longer readable
+           * @private
+           * @internal
+           * @param result
+           */
           const process = async (
             result: ReadableStreamReadResult<Uint8Array>,
           ): Promise<ReadableStreamReadResult<Uint8Array>> => {

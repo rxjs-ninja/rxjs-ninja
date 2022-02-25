ab6b96255be19335c71118cde13641a9344c88e4
Tane Piper <tane.piper@inter.ikea.com>
2022-02-25 23:43:46 +0100

    Simplifying lint?

--- .github/scripts/run-linting.sh
+++ .github/scripts/run-linting.sh
@@ -4,13 +4,15 @@ set -o errexit -o noclobber -o nounset -o pipefail
 RUN_ALL=${1:-"False"}
 BASE="main"
 
-echo "Running Linting"
-if [[ "$RUN_ALL" == "True" ]]; then
-  pnpm affected:lint --all
-else
-  AFFECTED=$(pnpx nx affected:libs --plain --base="$BASE")
-  echo "Will Lint: $AFFECTED"
-  pnpm affected:lint --base="$BASE"
-fi
-echo "Linting Complete"
-wait
+pnpm affected:lint
+
+# echo "Running Linting"
+# if [[ "$RUN_ALL" == "True" ]]; then
+#   pnpm affected:lint --all
+# else
+#   AFFECTED=$(pnpx nx affected:libs --plain --base="$BASE")
+#   echo "Will Lint: $AFFECTED"
+#   pnpm affected:lint --base="$BASE"
+# fi
+# echo "Linting Complete"
+# wait

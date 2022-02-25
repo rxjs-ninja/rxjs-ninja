38b0ae8a63ac45ae14e8ba1e9c70f2a01cf3b494
Tane Piper <tane.piper@inter.ikea.com>
2022-02-25 16:09:15 +0100

    Update repo dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/libs/rxjs/array',
-    '<rootDir>/libs/rxjs/boolean',
-    '<rootDir>/libs/rxjs/number',
-    '<rootDir>/libs/rxjs/string',
-    '<rootDir>/libs/rxjs/utility',
-    '<rootDir>/libs/rxjs/random',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
